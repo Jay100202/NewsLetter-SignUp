a41b00796d1f35b462e97c23bc793cd1d6fecb7e
Jay Mandaliya <108296276+clumsydemon@users.noreply.github.com>
2023-03-20 20:23:40 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req,res){
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/" , function(req,res){
+   
+    const firstName = req.body.fname;
+    const lastname = req.body.lname;
+    const email = req.body.email;
+
+    var data = {
+        members: [
+        {
+            email_address: email,
+            status:"subscribed",
+            merge_fields:{
+                FNAME: firstName,
+                LNAME: lastname,
+            }    
+        }
+     ]
+    };
+
+    var jsonData = JSON.stringify(data);
+
+    const url = "https://us21.api.mailchimp.com/3.0/lists/9247a9bec1";
+
+    const options = {
+        method: "POST",
+        auth:"Jay1:7a616a4fe07b53e335d7919bae8b8d81-us21"
+    }
+
+    const request = https.request(url,options,function(response){
+
+        if(response.statusCode === 200){
+            res.sendFile(__dirname + "/success.html");
+        }
+        else{
+            res.sendFile(__dirname + "/failure.html");
+        }
+
+        response.on("data",function(data){
+            console.log(JSON.parse(data));
+        })
+    })
+
+    request.write(jsonData);
+    request.end();
+
+});
+
+app.post("/failure",function(req,res){
+    res.redirect("/");
+});
+
+
+
+app.listen(3000,function(){
+    console.log("Server is running at port 3000");
+});
+
+
+//API KEY
+//7a616a4fe07b53e335d7919bae8b8d81-us21
+
+//List id
+//9247a9bec1
